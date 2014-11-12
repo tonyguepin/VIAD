9258c3f904f2e014c6d2b23091f5459086f04e63
erikdegraaf <erikdegraaf@91833aec-5511-0410-b5ed-e32adbd2bac9>
2014-11-12 18:07:12 +0000

    Test voor wijziging twee
    git-svn-id: https://svn.intergep.nl/svn-werk/Bestwerk/VIAD/trunk@3540 91833aec-5511-0410-b5ed-e32adbd2bac9

--- viad/wp-config-sample copy.php
+++ viad/wp-config-sample copy.php
@@ -22,7 +22,7 @@ define('DB_NAME', 'database_name_here');
 define('DB_USER', 'username_here');
 
 /** MySQL database password */
-define('DB_PASSWORD', '****');
+define('DB_PASSWORD', 'password_here**');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
