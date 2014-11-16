860f1e8fdac632d66f9c12d9349d83ddf0f25f75
hanspeeters <hanspeeters@91833aec-5511-0410-b5ed-e32adbd2bac9>
2014-11-16 21:56:14 +0000

    Registratie stap 1 styling en CSS
    git-svn-id: https://svn.intergep.nl/svn-werk/Bestwerk/VIAD/trunk@3569 91833aec-5511-0410-b5ed-e32adbd2bac9

--- viad/wp-content/themes/viad/stap-1.php
+++ viad/wp-content/themes/viad/stap-1.php
@@ -3,9 +3,72 @@
 get_header();
 ?>
 <section class="register">
-		<div class="container">
 
-		stap 1
+			<!--
+			<aside class="filter">
+			aside ja/nee?
+			</aside>
+			-->
+
+			<div class="register-titel">
+				<div class="container">
+					<h2>Aanmelden - stap 1</h2>
+					<div class="stappen">
+						<div class="stap-item selected">
+							Stap 1
+							<a href="#"><div class="stap-rectangle">&nbsp;</div></a>
+						</div>
+						<div class="stap-item">
+							Stap 2
+							<a href="#"><div class="stap-rectangle">&nbsp;</div></a>
+						</div>
+						<div class="stap-item">
+							Stap 3
+							<a href="#"><div class="stap-rectangle">&nbsp;</div></a>
+						</div>
+						<div class="stap-item">
+							Stap 4
+							<a href="#"><div class="stap-rectangle">&nbsp;</div></a>
+						</div>
+						<div class="stap-item last">
+							Stap 5
+							<a href="#"><div class="stap-rectangle">&nbsp;</div></a>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="register-kop">
+				<div class="container">
+					<h2>Ik ben ...</h2>	
+					<p>Maak hieronder een keuze uit een van de drie opties.</p>
+				</div>
+			</div>
+			
+			<div class="register-form">
+				<div class="container">
+					<fieldset class="stap1">
+						<div class="registratie-opties clearfix">
+							<div class="registratie-optie">
+								<div class="circle blue">&nbsp;</div>
+								<div class="radio"><input id="i0" type="radio" name="type" value="professionals" checked="checked"/><label for="i0">Professional</label></div>
+							</div>
+							<div class="registratie-spacer">of</div>
+							<div class="registratie-optie">
+								<div class="circle green">&nbsp;</div>
+								<div class="radio"><input id="i1" type="radio" name="type" value="companies"/><label for="i1">Ingenieursbureau</label></div>
+							</div>
+							<div class="registratie-spacer">of</div>
+							<div class="registratie-optie">
+								<div class="circle red">&nbsp;</div>
+								<div class="radio"><input id="i2" type="radio" name="type" value="companies"/><label for="i2">Bedrijf / overheid</label></div>
+							</div>
+						</div>					
+					</fieldset>								
+				</div>
+			</div>
+			<div class="register-volgende">
+				<a class="button register-button">Volgende stap</a>
+			</div>
 		</div>
 </section>
 
